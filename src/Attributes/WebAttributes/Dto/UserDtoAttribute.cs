fb317a3116ef960b04ca375e2143ff53c9f88890
Daniel Hufnagl <d.hufnagl@codelisk.com>
2023-07-29 19:28:51 +0200

    Added Attributes

---
+++ src/Attributes/WebAttributes/Dto/UserDtoAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attributes.WebAttributes.Dto
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class UserDtoAttribute
+    {
+    }
+}
