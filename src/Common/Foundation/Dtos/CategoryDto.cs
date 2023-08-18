50f117b70be3985081ca8eb7afe9f8331145b06b
Daniel Hufnagl <d.hufnagl@codelisk.com>
2023-08-18 12:35:40 +0200

    Minor

--- src/Common/Foundation/Dtos/CategoryDto.cs
+++ src/Common/Foundation/Dtos/CategoryDto.cs
@@ -4,7 +4,6 @@ using Foundation.Dtos.Base;
 namespace Foundation.Dtos
 {
     [Dto]
-    [GenerateAutoFilter]
     public partial class CategoryDto : BaseDto
     {
         public string Name { get; set; }
--- src/Common/Foundation/Dtos/ProductDto.cs
+++ src/Common/Foundation/Dtos/ProductDto.cs
@@ -4,7 +4,6 @@ using Foundation.Dtos.Base;
 namespace Foundation.Dtos
 {
     [Dto]
-    [GenerateAutoFilter]
     public class ProductDto : BaseDto
     {
         public string ProductName { get; set; }
