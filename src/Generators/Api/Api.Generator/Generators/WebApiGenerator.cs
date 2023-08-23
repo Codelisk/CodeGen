9c27dccd75689e098c231f500d9bc7fe59c44090
Daniel Hufnagl <d.hufnagl@codelisk.com>
2023-08-23 09:53:32 +0200

    Test again

--- src/Generators/Api/Api.Generator/Generators/WebApiGenerator.cs
+++ src/Generators/Api/Api.Generator/Generators/WebApiGenerator.cs
@@ -8,22 +8,19 @@ namespace Api.Generator.Generators
     [Generator]
     public class WebApiGenerator : BaseGenerator
     {
-        public static int line = 0;
+        public static string line = "";
         public override void Execute(GeneratorExecutionContext context)
         {
             try
             {
                 var refitApiCodeBuilder = new RefitApiCodeBuilder(context.Compilation.AssemblyName).Get(context);
-                line = 1;
+                line = "1";
                 var repositoriesCodeBuilder = new RepositoryCodeBuilder(context.Compilation.AssemblyName).Get(context);
-                line = 2;
+                line = "2";
                 var classServicesModuleInitializerBuilder = new ModuleInitializerBuilder(context.Compilation.AssemblyName).Get(context, repositoriesCodeBuilder);
 
-                line = 3;
                 AddSource(context, "Repositories", repositoriesCodeBuilder, ("abstract partial", "partial"));
-                line = 4;
                 AddSource(context, "Apis", refitApiCodeBuilder, ("abstract partial", "partial"));
-                line = 5;
                 AddSource(context, "", classServicesModuleInitializerBuilder);
                 //AddSource(context, "Repositories", repositoriesCodeBuilder);
             }
