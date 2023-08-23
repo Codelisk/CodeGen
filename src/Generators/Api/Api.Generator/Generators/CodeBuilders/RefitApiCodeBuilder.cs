55bd3a2101857f6d0e1264f25c99a576d5d4b226
Daniel Hufnagl <d.hufnagl@codelisk.com>
2023-08-23 18:13:35 +0200

    Add backslash to api

--- src/Generators/Api/Api.Generator/Generators/CodeBuilders/RefitApiCodeBuilder.cs
+++ src/Generators/Api/Api.Generator/Generators/CodeBuilders/RefitApiCodeBuilder.cs
@@ -53,7 +53,7 @@ namespace Api.Generator.Generators.CodeBuilders
                 var attributeUrl = context.AttributeUrl(attr.Key, dto);
                 var httpAttributeSymbol = context.GetClass(attr.Key, "Codelisk.GeneratorAttributes");
                 c.AddMethod(attributeUrl, Accessibility.Public).Abstract(true)
-                    .AddAttribute(attributeUrl.AttributeWithConstructor(attr.Value))
+                    .AddAttribute(attributeUrl.AttributeWithConstructor("/" + attr.Value))
                     .AddParametersForHttpMethod(httpAttributeSymbol, dto)
                     .WithReturnTypeForHttpMethod(attr.Key, dto);
             }
