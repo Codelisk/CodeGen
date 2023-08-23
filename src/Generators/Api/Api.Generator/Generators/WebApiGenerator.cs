d6ca0700740cdb3317104404bff5a8ce5841a9b1
Daniel Hufnagl <d.hufnagl@codelisk.com>
2023-08-23 17:41:53 +0200

    Remove debugger launch

--- src/Generators/Api/Api.Generator/Generators/WebApiGenerator.cs
+++ src/Generators/Api/Api.Generator/Generators/WebApiGenerator.cs
@@ -13,6 +13,7 @@ namespace Api.Generator.Generators
         {
             try
             {
+                //Debugger.Launch();
                 var refitApiCodeBuilder = new RefitApiCodeBuilder(context.Compilation.AssemblyName).Get(context);
                 var repositoriesCodeBuilder = new RepositoryCodeBuilder(context.Compilation.AssemblyName).Get(context);
                 var classServicesModuleInitializerBuilder = new ModuleInitializerBuilder(context.Compilation.AssemblyName).Get(context, repositoriesCodeBuilder);
