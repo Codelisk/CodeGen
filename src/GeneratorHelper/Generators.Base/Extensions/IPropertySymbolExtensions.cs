a4d52e2ba4097962a5eef3c0bec5b504db4e062b
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-04 11:04:48 +0100

    Fix codegenerator references

--- src/GeneratorHelper/Generators.Base/Extensions/IPropertySymbolExtensions.cs
+++ src/GeneratorHelper/Generators.Base/Extensions/IPropertySymbolExtensions.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.CodeAnalysis;
-using Codelisk.GeneratorShared.Constants;
 
 namespace Generators.Base.Extensions
 {
