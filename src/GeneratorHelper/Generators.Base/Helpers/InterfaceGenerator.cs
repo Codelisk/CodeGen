7422eec1d273266725e07f7595a14ed7f5a32e0c
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-17 11:15:20 +0100

    a

--- src/GeneratorHelper/Generators.Base/Helpers/InterfaceGenerator.cs
+++ src/GeneratorHelper/Generators.Base/Helpers/InterfaceGenerator.cs
@@ -55,7 +55,7 @@ namespace Generators.Base.Helpers
                 TestLog.Add("Method:" + publicMethod.Name);
                 result.AddMethod(publicMethod.Name, Accessibility.NotApplicable)
                     .AddParameters(publicMethod.Parameters)
-                    .WithReturnType(publicMethod.ReturnType.Name)
+                    .WithReturnType(publicMethod.ReturnType.GetNamespace())
                     .Abstract(true);
 
                 TestLog.Add("publicMethod.ReturnType:" + publicMethod.ReturnType);
