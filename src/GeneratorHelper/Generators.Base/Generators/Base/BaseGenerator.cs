4ccd4c53232f4255349b586dd9f54a8b5f425142
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-21 07:44:20 +0100

    AddSourceFileName

--- src/GeneratorHelper/Generators.Base/Generators/Base/BaseGenerator.cs
+++ src/GeneratorHelper/Generators.Base/Generators/Base/BaseGenerator.cs
@@ -124,5 +124,87 @@ namespace Generators.Base.Generators.Base
                 }
             });
         }
+        protected void AddSourceFileName(IncrementalGeneratorInitializationContext context, IncrementalValueProvider<List<(List<CodeBuilder> codeBuilder, string? folderName, (string, string)? replace)>> codeBuildersProvider)
+        {
+            context.RegisterSourceOutput(codeBuildersProvider, static (sourceProductionContext, codeBuildersTuples) =>
+            {
+                foreach (var codeBuilderTuple in codeBuildersTuples)
+                {
+                    var codeBuilders = codeBuilderTuple.Item1;
+                    string? folderName = codeBuilderTuple.Item2;
+                    var replace = codeBuilderTuple.Item3;
+
+                    foreach (var codeBuilder in codeBuilders)
+                    {
+                        string code = codeBuilder.Build();
+                        if (replace.HasValue)
+                        {
+                            code = code.Replace(replace.Value.Item1, replace.Value.Item2);
+                        }
+
+                        if (codeBuilder.Classes.Any())
+                        {
+                            //Workaround because i cant make Interface methods
+                            if (codeBuilder.Classes.Any(x => x.Kind == TypeKind.Interface))
+                            {
+                                code = code.Replace("abstract ", "");
+                                code = code.Replace("using <global namespace>;", "");
+                            }
+
+                            code = code.Replace("internal void partial", "partial void");
+                            code = code.Replace("void partial", "partial void");
+
+                            //Workaround for interface generation in pipeline always generating false returntype for AddRange in GenerateInterface
+                            string pattern = @"System\+Collections\+Generic\+List`1\[(.*?)\]";
+                            string replacement = "List<$1>";
+
+                            string corrected = Regex.Replace(code, pattern, replacement);
+
+                            try
+                            {
+                                var fileName = code;
+
+                                fileName = fileName.Replace(" ", "leerzf")
+.Replace(":", "dopsspel")
+.Replace(";", "Strichpunkt")
+.Replace("@", "atteit")
+.Replace("{", "Kliaamerauf")
+.Replace("[", "eckiigauf")
+.Replace("}", "Klammerzu")
+.Replace("|", "Odöerdd")
+.Replace("]", "eclkigzua")
+.Replace("<", "aufkssa")
+.Replace(">", "zusjeee")
+.Replace(",", "Beihspf")
+.Replace("=", "gleigchhh")
+.Replace("_", "underrrrr")
+.Replace("(", "radsfsaaf")
+.Replace(")", "rzuwdsf")
+.Replace("/", "backaslash")
+.Replace("-", "bindaddd")
+.Replace("?", "frageee")
+.Replace("!", "rufffff")
+.Replace("\r\n", "neuezeile")
+.Replace("\n", "neuezeile") + ".g.cs";
+
+
+                                if (string.IsNullOrEmpty(folderName))
+                                {
+                                    sourceProductionContext.AddSource(fileName, code);
+                                }
+                                else
+                                {
+                                    sourceProductionContext.AddSource(folderName + "/" + fileName, code);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                sourceProductionContext.AddSource(folderName + "/Failed", ex.Message + " \n\nStacktrace:" + ex.StackTrace);
+                            }
+                        }
+                    }
+                }
+            });
+        }
     }
 }
